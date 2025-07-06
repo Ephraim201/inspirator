d79e54a2a0404143c5154731ef5b76b43bc30f82
Ephraim201 <williampena5336@gmail.com>
2025-07-06 18:30:46 +0200

    💡 Arreglo logo 4

--- src/data/autores.js
+++ src/data/autores.js
@@ -1,31 +1,31 @@
 const autores = [
   {
     nombre: 'Albert Einstein',
-    imagen: "/inspirator/img/albert.png"
+    imagen: "/img/albert.png"
   },
   {
     nombre: 'El tio Paco',
-    imagen: "/inspirator/img/paco.png"
+    imagen: "/img/paco.png"
   },
   {
     nombre: 'El bicho',
-    imagen: "/inspirator/img/bicho.png"
+    imagen: "/img/bicho.png"
   },
   {
     nombre: 'Hannibal Lecter',
-    imagen: "/inspirator/img/hanibal.png"
+    imagen: "/img/hanibal.png"
   },
   {
     nombre: 'La Roca',
-    imagen: "/inspirator/img/roca.png"
+    imagen: "/img/roca.png"
   },
   {
     nombre: 'Messi',
-    imagen: "/inspirator/img/messi.png"
+    imagen: "/img/messi.png"
   },
   {
     nombre: 'Jordi Wild',
-    imagen: "/inspirator/img/jordi.png"
+    imagen: "/img/jordi.png"
   }
 ]
 
