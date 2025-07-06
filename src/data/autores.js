c31d4470b04c4dddf92328af173989c6c8193f2b
Ephraim201 <williampena5336@gmail.com>
2025-07-06 15:09:09 +0200

    💡 Arreglo de imagenes3

--- src/data/autores.js
+++ src/data/autores.js
@@ -1,31 +1,31 @@
 const autores = [
   {
     nombre: 'Albert Einstein',
-    imagen: `${import.meta.env.BASE_URL}albert.png`
+    imagen: `${import.meta.env.BASE_URL}/albert.png`
   },
   {
     nombre: 'El tio Paco',
-    imagen: `${import.meta.env.BASE_URL}paco.png`
+    imagen: `${import.meta.env.BASE_URL}/paco.png`
   },
   {
     nombre: 'El bicho',
-    imagen: `${import.meta.env.BASE_URL}bicho.png`
+    imagen: `${import.meta.env.BASE_URL}/bicho.png`
   },
   {
     nombre: 'Hannibal Lecter',
-    imagen: `${import.meta.env.BASE_URL}hanibal.png`
+    imagen: `${import.meta.env.BASE_URL}/hanibal.png`
   },
   {
     nombre: 'La Roca',
-    imagen: `${import.meta.env.BASE_URL}roca.png`
+    imagen: `${import.meta.env.BASE_URL}/roca.png`
   },
   {
     nombre: 'Messi',
-    imagen: `${import.meta.env.BASE_URL}messi.png`
+    imagen: `${import.meta.env.BASE_URL}/messi.png`
   },
   {
     nombre: 'Jordi Wild',
-    imagen: `${import.meta.env.BASE_URL}jordi.png`
+    imagen: `${import.meta.env.BASE_URL}/jordi.png`
   }
 ]
 
